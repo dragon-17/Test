528b74c48824e72236882f0684dcb12965a8741b
dragon-17 <danielkarl.reul@gmail.com>
2024-07-23 11:30:56 +0200

    Datum

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+/** @param {String} sel   @returns {HTMLElement}*/
+function $(sel){return document.querySelector.bind(document);}
+
 console.log("🐉🐉🐉")
+
+$("div").innerHTML+=new Date().toLocaleDateString()
